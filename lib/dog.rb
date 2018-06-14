1ede81f4cc4005faffc83221c80bf13803dc5722
Vicki Hamm <vicki.cello@gmail.com>
2018-06-14 18:59:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog
-  def initialize(breed="Mutt")
-    @breed = breed
+  def initialize(name)
+    @name = name
   end
 
-  def breed=(breed)
-    @breed = breed
+  def name=(name)
+    @name = name
   end
 
-  def breed
-    @breed
+  def name
+    @name
   end
 end
